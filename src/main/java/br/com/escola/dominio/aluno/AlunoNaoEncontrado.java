54509a145d72a9b72db28cd113c221b72fa900e4
mychelle-comput <mychelinha7@hotmail.com>
2024-01-09 12:58:15 -0300

    commit inicial

--- src/main/java/br/com/escola/dominio/aluno/AlunoNaoEncontrado.java
+++ src/main/java/br/com/escola/dominio/aluno/AlunoNaoEncontrado.java
@@ -1,9 +1,13 @@
 package br.com.escola.dominio.aluno;
 
-public class AlunoNaoEncontrado extends Throwable {
+public class AlunoNaoEncontrado extends RuntimeException {
+
     private static final long serialVersionUID = 1L;
 
     public AlunoNaoEncontrado(CPF cpf) {
         super("Aluno nao encontrado com CPF: " + cpf.getNumero());
     }
+
 }
+
+
